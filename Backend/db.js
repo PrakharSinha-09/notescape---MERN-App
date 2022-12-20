f8dcffff1640ae0fc6e2bbb77bbaadd3abec3b34
Prakhar Sinha <prakhar.sinha2k2@gmail.com>
2022-12-20 19:31:24 +0530

    Connected Our DataBase

---
+++ Backend/db.js
@@ -0,0 +1,12 @@
+//Lets Get Connected To The MongoDb Server
+const mongoose=require('mongoose');               //it is not a ES6 way of importing!
+
+const mongoURI="mongodb://localhost:27017"
+
+const connectToMongo=()=>{
+    mongoose.connect(mongoURI,()=>{
+        console.log("Connected To MongoDB Successfully!")
+    })
+}
+
+module.exports=connectToMongo
---
+++ Backend/index.js
@@ -0,0 +1,16 @@
+//This index.js will be our express server
+
+const connectToMongo=require('./db')
+
+const express = require('express')
+connectToMongo()
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
